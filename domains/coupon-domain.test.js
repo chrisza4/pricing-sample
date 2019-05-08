98b5067e71eca8b773398320f184841747ae1baa
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2019-05-08 23:07:03 +0700

    Add a bunch

--- domains/coupon-domain.test.js
+++ domains/coupon-domain.test.js
@@ -1,7 +1,27 @@
+const CouponDomain = require('./coupon-domain')
 describe('CouponDomain', () => {
+  const normalCoupon = {
+    code: 'hi',
+    type: 'percent',
+    expired_at: new Date('2019-01-01')
+  }
   describe('validCoupon', () => {
-    it('should return false for wrong coupon type', () => {
+    it('should return true for normal coupon type', () => {
+      expect(CouponDomain.validCoupon(normalCoupon)).toBeTruthy()
+    })
 
+    it('should return false for wrong coupon type', () => {
+      expect(CouponDomain.validCoupon({
+        ...normalCoupon,
+        type: 'random'
+      })).toBeFalsy()
+    })
+
+    it('should return false for non-expired coupon', () => {
+      expect(CouponDomain.validCoupon({
+        ...normalCoupon,
+        expired_at: null
+      })).toBeFalsy()
     })
   })
 })
